33ee1c150cce81bd74c08fefa1afa8a3ad9b47b5
Ptmlol <ptmlol90@yahoo.com>
2024-06-16 15:34:12 +0300

    added multithreading

--- WebScanAssist/Scanner.py
+++ WebScanAssist/Scanner.py
@@ -1,3 +1,6 @@
+import math
+from datetime import time
+
 from colorama import Fore
 import warnings
 from bs4 import MarkupResemblesLocatorWarning
@@ -9,6 +12,7 @@ from Tests import InjectionIframe, InjectionSql, InjectionCodeExec, InjectionPhp
     BrokenAuthRoleDefDir, BrokenAuthRoleDefCookie, InjectionXss, InjectionIdor, MisconfigCors, InjectionSsrf, \
     InjectionXml, InjectionLfi, InfoComments, InjectionHtml, BrokenAuthSession, MisconfigBrowserCache, MisconfigXst, \
     MisconfigHhi, MisconfigHttp, MisconfigHsts, MisconfigRia, MisconfigRobots, InjectionJs
+import threading
 
 warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
 
@@ -25,68 +29,18 @@ class Scanner(Utilities):
     def scan(self):
         try:
             # Scan app
-            if self.config_params['TEST']['browser_cache_tests']:
-                MisconfigBrowserCache.run(self.url)
-            if self.config_params['TEST']['xst_tests']:
-                MisconfigXst.run(self.url)
-            if self.config_params['TEST']['hhi_tests']:
-                MisconfigHhi.run(self.url)
-            if self.config_params['TEST']['http_tests']:
-                MisconfigHttp.run(self.url)
-            if self.config_params['TEST']['hsts_tests']:
-                MisconfigHsts.run(self.url)
-            if self.config_params['TEST']['ria_tests']:
-                MisconfigRia.run(self.url)
-            if self.config_params['TEST']['robots_tests']:
-                MisconfigRobots.run(self.url, self.static_scan)
+            Scanner.app_scan(self.url)
+            threads_main = []
 
-            # Scan harvested URLs.
-            for url in self.DataStorage.urls:
+            # Scan harvested URLs in sub_lists.
+            for url_set in list(Scanner.split_list(list(self.DataStorage.urls),math.ceil(len(list(self.DataStorage.urls))/5))):
+                threads_main.append(threading.Thread(target=Scanner.run_thread, args=(url_set,)))
 
-                print("\nTesting URL: ", url)
-                # # # Injections: # TODO: One of the 4 below is VERY slow
-                if Utilities.str_bool(self.config_params['TEST']['sql_tests']):
-                    InjectionSql.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['iframe_tests']):
-                    InjectionIframe.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['code_execution_tests']):
-                    InjectionCodeExec.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['php_execution_tests']):
-                    InjectionPhpExec.run(url)
+            for thread in threads_main:
+                thread.start()
 
-
-                if Utilities.str_bool(self.config_params['TEST']['ssi_tests']):
-                    InjectionSsi.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['xss_tests']):
-                    InjectionXss.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['idor_tests']):
-                    InjectionIdor.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['ssrf_tests']):
-                    InjectionSsrf.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['xml_tests']):
-                    InjectionXml.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['lfi_tests']):
-                    InjectionLfi.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['js_tests']):
-                    InjectionJs.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['html_tests']):
-                    InjectionHtml.run(url)
-
-                # Broken Authentications:
-                if Utilities.str_bool(self.config_params['TEST']['ba_role_def_dir_tests']):
-                    BrokenAuthRoleDefDir.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['ba_role_def_cookie_tests']):
-                    BrokenAuthRoleDefCookie.run(url)
-                if Utilities.str_bool(self.config_params['TEST']['ba_session_tests']):
-                    BrokenAuthSession.run(url)
-
-                # Security Misconfigurations:
-                if Utilities.str_bool(self.config_params['TEST']['cors_tests']):
-                    MisconfigCors.run(url)
-
-                #Misc
-                if Utilities.str_bool(self.config_params['TEST']['comments_tests']):
-                    InfoComments.run(url)
+            for thread in threads_main:
+                thread.join()
 
             return
         except Exception as e:
@@ -95,6 +49,99 @@ class Scanner(Utilities):
             quit()
 
 
+    @staticmethod
+    def app_scan(url):
+        # Generic app tests over whole app. 1 base URL. # TODO: Get the smallest URL harvested (domain of the scanned app).
+        if Scanner.config_params['TEST']['browser_cache_tests']:
+            MisconfigBrowserCache.run(url)
+        if Scanner.config_params['TEST']['xst_tests']:
+            MisconfigXst.run(url)
+        if Scanner.config_params['TEST']['hhi_tests']:
+            MisconfigHhi.run(url)
+        if Scanner.config_params['TEST']['http_tests']:
+            MisconfigHttp.run(url)
+        if Scanner.config_params['TEST']['hsts_tests']:
+            MisconfigHsts.run(url)
+        if Scanner.config_params['TEST']['ria_tests']:
+            MisconfigRia.run(url)
+        if Scanner.config_params['TEST']['robots_tests']:
+            MisconfigRobots.run(url, Scanner.static_scan)
+
+    @staticmethod
+    def thread_time_1(url):
+        # Injection - balanced time execution
+        if Utilities.str_bool(Scanner.config_params['TEST']['php_execution_tests']):
+            InjectionPhpExec.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['ssrf_tests']):
+            InjectionSsrf.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['xml_tests']):
+            InjectionXml.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['lfi_tests']):
+            InjectionLfi.run(url)
+
+    @staticmethod
+    def thread_time_2(url):
+        if Utilities.str_bool(Scanner.config_params['TEST']['idor_tests']):
+            InjectionIdor.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['js_tests']):
+            InjectionJs.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['html_tests']):
+            InjectionHtml.run(url)
+
+    @staticmethod
+    def thread_time_3(url):
+        if Utilities.str_bool(Scanner.config_params['TEST']['sql_tests']):
+            InjectionSql.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['iframe_tests']):
+            InjectionIframe.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['ba_role_def_dir_tests']):
+            BrokenAuthRoleDefDir.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['cors_tests']):
+            MisconfigCors.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['comments_tests']):
+            InfoComments.run(url)
+
+    @staticmethod
+    def thread_time_4(url):
+        if Utilities.str_bool(Scanner.config_params['TEST']['code_execution_tests']):
+            InjectionCodeExec.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['ba_role_def_cookie_tests']):
+            BrokenAuthRoleDefCookie.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['ba_session_tests']):
+            BrokenAuthSession.run(url)
+
+    @staticmethod
+    def thread_time_5(url):
+        if Utilities.str_bool(Scanner.config_params['TEST']['ssi_tests']):
+            InjectionSsi.run(url)
+        if Utilities.str_bool(Scanner.config_params['TEST']['xss_tests']):
+            InjectionXss.run(url)
+
+    @staticmethod
+    def run_thread(lst):
+        for url in lst:
+            threads = []
+            print("\nTesting URL: ", url)
+            # Create threads for each function
+
+            threads.append(threading.Thread(target=Scanner.thread_time_1, args=(url,)))
+            threads.append(threading.Thread(target=Scanner.thread_time_2, args=(url,)))
+            threads.append(threading.Thread(target=Scanner.thread_time_3, args=(url,)))
+            threads.append(threading.Thread(target=Scanner.thread_time_4, args=(url,)))
+            threads.append(threading.Thread(target=Scanner.thread_time_5, args=(url,)))
+
+            # Start each thread
+            for thread in threads:
+                thread.start()
+
+            # Wait for all threads to complete
+            for thread in threads:
+                thread.join()
+
+    @staticmethod
+    def split_list(lst, n):
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
 # class CreateUserSession(Utilities):  # TODO: Create another py module for the new user.
 #     try:
 #         def __init__(self, url):
