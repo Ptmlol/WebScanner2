1789fd45ac0f84b2e8f988f5b4707220c5958ffb
Ptmlol <ptmlol90@yahoo.com>
2024-07-25 18:06:24 +0300

    added CVE to Report. Need to beautify and fix duplicate bug

--- WebScanAssist/CustomImports/html_report.py
+++ WebScanAssist/CustomImports/html_report.py
@@ -11,12 +11,12 @@ external_links = set()
 non_accessible_links = set()
 comments_dict = {}
 hidden_paths = []
+cve_dict = []
 
 try:
     def add_vulnerability(v_type, details, confidence='Critical', payload=None, reply=None, comment=None):
         vulnerabilities.append({'type': v_type, 'details': details, 'severity': confidence, 'dropdown_details': {'payload': payload, 'reply': reply, 'comment': comment}})
         return
-
 
     def create_tree(tree_list):
         try:
@@ -26,30 +26,38 @@ try:
         except Exception as e:
             print(e)
 
-
     def add_external_link(external):
         global external_links
         external_links.add(external)
         return
-
 
     def add_non_accessible_link(external):
         global non_accessible_links
         non_accessible_links.add(external)
         return
 
-
     def add_comments(external):
         global comments_dict
         comments_dict.update(external)
         return
-
 
     def add_hidden_path(path, status_code):
         global hidden_paths
         hidden_paths.append({'path': path, 'status_code': status_code})
         return
 
+    def add_cve(app_name_supp, app_name_prod, cve_id, cve_description, cve_url,cve_score, cve_severity):
+        global cve_dict
+        cve_dict.append({
+            'app_name_supp': app_name_supp,
+            'app_name_prod': app_name_prod,
+            'cve_id': cve_id,
+            'cve_description': cve_description,
+            'cve_url': cve_url,
+            'cve_score': cve_score,
+            'cve_severity': cve_severity
+        })
+        return
 
     def write_html_report():
         try:
@@ -58,7 +66,7 @@ try:
             template = env.get_template('report.html')
             # Render the template with the data
             output = template.render(vulnerabilities=vulnerabilities, tree_json=tree_json, external_links=external_links, non_accessible_links=non_accessible_links,
-                                     comments_dict=comments_dict, hidden_paths=hidden_paths)
+                                     comments_dict=comments_dict, hidden_paths=hidden_paths, cve_dict=cve_dict)
             # Save the output to a file
             # with open('Vulnerability_Report_' + datetime.now().strftime('%Y-%m-%d_%H-%M') + '.html', 'w', encoding='utf-8') as f:
             with open('report.html', 'w', encoding='utf-8') as f:  # temp until on prod.
--- WebScanAssist/Tests/InfoVersionFinder.py
+++ WebScanAssist/Tests/InfoVersionFinder.py
@@ -1,5 +1,8 @@
 import html
 import warnings
+import ast
+from CustomImports import html_report
+
 warnings.simplefilter("ignore", UserWarning)
 import requests
 from Classes.ScanConfig import ScanConfig
@@ -24,24 +27,38 @@ def get_apps(url):
 def get_cpes(app_name, version):
     API_KEY = ScanConfig.config_params['API']['API_KEY']
     try:
-        cpe_list = []
+        cpe_list = set()
         cpes = nvdlib.searchCPE(keywordSearch=app_name, key=API_KEY ,delay=1)
         for cpe in cpes:
-            if (':' + str(version) + ':') in str(cpe.cpeName): # PHP 5.2
-                cpe_list.append(cpe.cpeName)
+            cpe_array = str(cpe.cpeName).split(':')
+            if (':' + str(version) + ':') in str(cpe.cpeName) and (cpe_array[3] == app_name or cpe_array[4] == app_name):
+                cpe_list.add(cpe.cpeName)
         return cpe_list
     except Exception as e:
         Utilities.print_except_message('error', e,
                                        "Something went wrong when getting CPEs. Passing..")
         pass
 
+def get_cve(cpe_name):
+    API_KEY = ScanConfig.config_params['API']['API_KEY']
+    try:
+        cve_dict = {}
+        cves = nvdlib.searchCVE(cpeName=cpe_name, limit=5, key=API_KEY, delay=1)
+        for cve in cves:
+            cve_dict[cpe_name] = [str(cve.id), html.unescape(str(cve.descriptions)), str(cve.url), str(cve.score)]
+        return cve_dict
+    except Exception as e:
+        Utilities.print_except_message('error', e,
+                                       "Something went wrong when getting CVEs. Passing..")
+        pass
+
 def run(url):
     try:
         app_dict = {}
+        cve_dict = {}
         cpe_list = []
         non_version_app_list = []
         total_apps = get_apps(url)
-        print(total_apps)
         for app_name, version in total_apps.items():
             app_dict[app_name] = str(total_apps[app_name]['versions']).replace('[', '').replace(']', '').replace("'", '')
         for app_name, version in app_dict.items():
@@ -51,6 +68,24 @@ def run(url):
                     cpe_list.extend(cpe)
             else:
                 non_version_app_list.append(app_name)
-        print(cpe_list)
+        for cpeName in cpe_list: # Remove duplicates
+            cve_dict.update(get_cve(cpeName))
+
+        for name, dict_array in cve_dict.items():
+            cpe_array = str(name).split(':')
+            app_name_supp = cpe_array[3]
+            app_name_prod = cpe_array[4]
+            cve_id = dict_array[0]
+            dict_array[1] = ast.literal_eval(dict_array[1])
+            dict_array[1][0] = ast.literal_eval(str(dict_array[1][0]))
+            cve_description = str(dict_array[1][0]['value']).replace('\n', '')
+            cve_url = dict_array[2]
+            dict_array[3] = ast.literal_eval(dict_array[3])
+            cve_score = dict_array[3][1]
+            cve_severity = dict_array[3][2]
+
+            html_report.add_cve(app_name_supp, app_name_prod, cve_id, cve_description, cve_url, cve_score, cve_severity)
     except Exception as e:
-        print(e)
+        Utilities.print_except_message('error', e,
+                                       "Something went wrong when getting CPEs. Passing..")
+        pass
